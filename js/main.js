c484d1cb3d0f8f39cf21da5e279e5a5d44987d02
austincarvey <austin@carvey.me>
2016-01-09 14:29:27 -0500

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-var markers = {};
-var count = 0;
-function makeMarker(lat,lng){
-  markers[count] = new google.maps.Marker({
-  	 		position: {'lat':lat,'lng':lng},
-  	 		map: map,
-  	 		title: "" + count,
-  		});
-  count += 1;
+function pickBook(){
+
 }
+
+function parseForLocations(){
+
+}
+
+function markLocations(){
+
+}
+
+function startJourney(){
+
+}
+
+// pick book / enter text
+// parse for locations
+// mark locations
+// go to first location, the go to others sequentially
--- js/map.js
+++ js/map.js
@@ -9,3 +9,15 @@ function initialize() {
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 initialize();
+
+var markers = {};
+var count = 0;
+
+function makeMarker(lat,lng){
+  markers[count] = new google.maps.Marker({
+  	 		position: {'lat':lat,'lng':lng},
+  	 		map: map,
+  	 		title: "" + count,
+  		});
+  count += 1;
+}
